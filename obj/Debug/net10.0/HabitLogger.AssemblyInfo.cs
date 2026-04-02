93e0cc227ace67d2bf5e72499514a2a0e8b7a60e
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-04-03 01:44:02 +0200

    run the program and some files were created and updated

--- obj/Debug/net10.0/HabitLogger.AssemblyInfo.cs
+++ obj/Debug/net10.0/HabitLogger.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+83c57946043c22b777f2f5e46dbd5e349bc1d663")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5bd746dda1a8461bcb0a233a4b6f08a05c0a5cd6")]
 [assembly: System.Reflection.AssemblyProductAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HabitLogger")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
