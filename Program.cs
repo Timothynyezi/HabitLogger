78c768fed9946c0eea7535ca1d88f81462bcbaaa
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-04-03 00:49:54 +0200

    created a constructor to read when reading rows form the database

--- Program.cs
+++ Program.cs
@@ -1,6 +1,13 @@
 ﻿class HabitEntry
 {
-    public int ID { get; set; }
+    public int Id { get; set; }
     public string Date { get; set; }
     public int Quantity { get; set;}
+
+    public HabitEntry(int id, string date, int quantity)
+    {
+        Id = id;
+        Date = date;
+        Quantity = quantity;
+    }
 }
