618d948cc483b1c5fe367f682c571d3f77ad70c2
Thamsanqa <thnyezijhb024@student.wethinkcode.co.za>
2026-04-03 01:25:12 +0200

    wrote some code

--- Program.cs
+++ Program.cs
@@ -1,13 +1,52 @@
-﻿class HabitEntry
-{
-    public int Id { get; set; }
-    public string Date { get; set; }
-    public int Quantity { get; set;}
+﻿using System.Reflection.Metadata.Ecma335;
 
-    public HabitEntry(int id, string date, int quantity)
+DatabaseManager db = new DatabaseManager();
+
+db.InitialiseDatabase();
+
+bool isRunning = true;
+
+ShowWelcome();
+
+while (isRunning)
+{
+    Console.Clear();
+    Console.WriteLine("╔══════════════════════════════╗");
+    Console.WriteLine("║       HABIT LOGGER           ║");
+    Console.WriteLine("║    💧 Water Intake Tracker   ║");
+    Console.WriteLine("╠══════════════════════════════╣");
+    Console.WriteLine("║  1.  Log Water Intake        ║");
+    Console.WriteLine("║  2.  View All Entries        ║");
+    Console.WriteLine("║  3.  Update an Entry         ║");
+    Console.WriteLine("║  4.  Delete an Entry         ║");
+    Console.WriteLine("║  0.  Exit                    ║");
+    Console.WriteLine("╚══════════════════════════════╝");
+    Console.Write("\n  Choose an option: "); 
+
+    string choice = Console.ReadLine()?.Trim();
+
+    switch (choice)
     {
-        Id = id;
-        Date = date;
-        Quantity = quantity;
+        case "1":
+            LogEntry(db);
+            break;
+        case "2":
+            ViewEntries(db);
+            break;
+        case "3":
+            UpdateEntry(db);
+            break;
+        case "4":
+            DeleteEntry(db);
+            break;
+        case "0":
+            isRunning = false;
+            ShowGoodby();
+            break;
+        default:
+        Console.WriteLine("\n Invalid option. Please choose 0 -4.");
+
     }
+
+    
 }
